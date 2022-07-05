39f23c1896e7973e4d65d0c997fd1f11a7fde7cd
Simone Vazzoler <svazzole@gmail.com>
2022-07-05 07:16:00 +0200

    Some little progress

--- newsrss/__main__.py
+++ newsrss/__main__.py
@@ -1,39 +1,78 @@
 import os
-from typing import Optional
+from typing import List, Optional
 
 from gnews import GNews
 from rich import print
+from rich.console import Group, group
 from rich.panel import Panel
 from rich.text import Text
 from textual.app import App
 from textual.reactive import Reactive
 from textual.widget import Widget
-from textual.widgets import Button, Footer, Header, Placeholder, ScrollView
+from textual.widgets import Footer, Header, ScrollView
+
+
+@group()
+def get_news_titles(titles):
+    for tits in titles:
+        yield Title(title=tits)
+
+
+class Title(Widget):
+    mouse_over = Reactive(False)
+
+    def __init__(
+        self,
+        title: str,
+        name: Optional[str] = None,
+    ) -> None:
+        super().__init__(name=name)
+        self.title = title
+
+    def render(self):
+        news_title = self.title.split(" - ")[0]
+        source = self.title.split(" - ")[1]
+        new_title = f"{source} ||| {news_title}"
+        title_text = Text(
+            new_title,
+        )
+        title_text.stylize("bold magenta", 0, len(source) + 2)
+        title_text.stylize("bold white", len(source) + 2, len(source) + 3)
+        title_text.stylize("bold magenta", len(source) + 3, len(source) + 4)
+        return Panel(title_text, style=("on blue" if self.mouse_over else ""))
+
+    def on_enter(self) -> None:
+        self.mouse_over = True
+
+    def on_leave(self) -> None:
+        self.mouse_over = False
 
 
 class NewsTitles(Widget):
-    def __init__(self, name: Optional[str] = None, titles=None) -> None:
+    def __init__(
+        self,
+        titles: List[str],
+        name: Optional[str] = None,
+    ) -> None:
         super().__init__(name=name)
         self.titles = titles
 
     def render(self):
-        page = Text(end="")
-        for title in self.titles:
-            page.append_text(Text(title))
-            page.append_text(Text("\n\n"))
-
-        page.append_text(Text("<- Prev Page 0/0 Next ->"))
-        return Panel(page, title="News", expand=False)
+        return Panel(get_news_titles(self.titles), title="Latest News")
 
 
 class NewsArticle(Widget):
-    def __init__(self, name: Optional[str] = None, article=None) -> None:
+    def __init__(
+        self,
+        article: str,
+        name: Optional[str] = None,
+    ) -> None:
         super().__init__(name=name)
         self.article = article
 
     def render(self):
         page = Text(text=self.article, end="")
-        return Panel(page, title="Article", expand=False)
+        return Panel(page, title="Article", expand=True, style="white on black")
 
 
 class NewsRSSApp(App):
@@ -45,7 +84,10 @@ class NewsRSSApp(App):
         await self.bind("q", "quit", "Quit")
 
         try:
-            self.titles, self.article = get_news()  # ["Titulo uni", "Titulo dui"]
+            self.titles, self.article = [
+                "Titulo uni - Sorcio 1",
+                "Titulo dui - Sorcio 2",
+            ], "Ciccio pasticcio"  # get_news()  #
         except Exception:
             self.titles = ["Some error occurred"]
 
@@ -53,18 +95,13 @@ class NewsRSSApp(App):
         """Call after terminal goes in to application mode"""
         news_titles = NewsTitles(titles=self.titles)
         x = os.get_terminal_size()[0]
-        print(x)
-        # await self.view.dock(Header(tall=False), edge="top")
-        # await self.view.dock(Footer(), edge="bottom")
-        # await self.view.dock(ScrollView(news_titles), edge="left")
-        # await self.view.dock(Placeholder(name="Placeholder for text"), edge="right")
 
         await self.view.dock(Header(tall=False), edge="top")
         await self.view.dock(Footer(), edge="bottom")
-        await self.view.dock(ScrollView(news_titles), edge="left", size=percent(50, x))
+        await self.view.dock(ScrollView(news_titles), edge="top", size=percent(50, x))
         await self.view.dock(
-            NewsArticle(name=None, article=self.article),
-            edge="right",
+            ScrollView(NewsArticle(name=None, article=self.article)),
+            edge="bottom",
             size=percent(50, x),
         )
 
